fc822752f364d85f752ef88df495efcd1ba3d898
ccwwonebyone <ccwwonebyone.163.com>
2020-03-29 14:14:20 +0800

    规范代码

--- creatReadme.py
+++ creatReadme.py
@@ -1,42 +1,44 @@
 # -*- coding: utf-8 -*-
 import os
 
+
 class ListDir:
-
-    listDir = './'  #遍历目录 当前目录
+    # 遍历目录 当前目录
+    listDir = './'
     fileUrl = 'https://github.com/{}/{}/blob/{}/'
-    dirUrl  = 'https://github.com/{}/{}/tree/{}/'
-    hearder = '''## [notebook](https://github.com/{}) '''
-    space   = '&nbsp; &nbsp;'
-    mark    = '&gt;'
+    dirUrl = 'https://github.com/{}/{}/tree/{}/'
+    header = '''## [notebook](https://github.com/{}) '''
+    space = '&nbsp; &nbsp;'
+    mark = '&gt;'
     username = ''
-    passFile = ['png','jpg','gif']
+    passFile = ['png', 'jpg', 'gif']
     rn = "\r\n"
-    def __init__(self,username,repository,branch):
-        self.username = username
-        self.fileUrl  = self.fileUrl.format(username,repository,branch)
-        self.dirUrl   = self.dirUrl.format(username,repository,branch)
-        self.hearder  = self.hearder.format(username)
 
-    def getAllFile(self,path):
+    def __init__(self, username, repository, branch):
+        self.username = username
+        self.fileUrl = self.fileUrl.format(username, repository, branch)
+        self.dirUrl = self.dirUrl.format(username, repository, branch)
+        self.header = self.header.format(username)
+
+    def get_all_file(self, path):
         files = []
         for file in os.listdir(path):
             dirname = path.replace('./','')
             if file != '.git' and file != '.gitignore':
                 if os.path.isdir(path+file):
-                    files.append({dirname+file:self.getAllFile(path+file+'/')})
+                    files.append({dirname+file:self.get_all_file(path+file+'/')})
                 else:
                     files.append(dirname+file)
 
         return files
 
-    def parseFiles(self,files,level):
+    def parse_files(self, files, level):
         string = ''
         for file in files:
             if type({}) == type(file):
                 name = [name for name in file][0]
                 string += self.rn+'>'+'#'*level+' '+self.mark*(level-2)+' ['+os.path.basename(name)+']'+'('+self.dirUrl+name+')'
-                string += self.rn+self.parseFiles(file[name],level+1)
+                string += self.rn+self.parse_files(file[name], level+1)
             else:
                 fileType = file.split('.')[-1]
                 if fileType in self.passFile:
@@ -45,20 +47,21 @@ class ListDir:
                     string += self.rn+'>'+'#'*level+' '+self.mark*(level-2)+' ['+os.path.basename(file)+']'+'('+self.fileUrl+file+')'
         return string
 
-    def creatReadme(self):
-        files = self.getAllFile(self.listDir)
+    def creat_readme(self):
+        files = self.get_all_file(self.listDir)
         string = '# 自己的一些学习记录，以及感慨' + self.rn
-        string += self.hearder + self.rn
-        string += self.parseFiles(files,3)
-        hander = open('./readme.md','w')
-        hander.write(string)
-        hander.close()
+        string += self.header + self.rn
+        string += self.parse_files(files, 3)
+        handler = open('./readme.md', 'w')
+        handler.write(string)
+        handler.close()
+
 
 if __name__ == '__main__':
     username = 'ccwwonebyone'
     repository = 'notebook'
     branch = 'master'
-    listdir = ListDir(username,repository,branch)
-    listdir.creatReadme()
+    listdir = ListDir(username, repository, branch)
+    listdir.creat_readme()
     print('已更新'+username+'的'+repository+'仓库的'+branch+'分支的'+'readme.md文件')
     input('回车退出')
