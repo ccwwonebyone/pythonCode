dc6317022932cbd9874a9a30d7c55920fb5e9268
ccwwonebyone <ccwwonebyone.163.com>
2020-03-29 14:59:35 +0800

    BuildMysqlDoc

--- build_mysql_doc.py
+++ build_mysql_doc.py
@@ -2,15 +2,15 @@
 
 import pymysql,os
 
+
 class BuildMysqlDoc:
-    def __init__(self,host,user,passwd,db,charset='utf8',port=3306):
+    def __init__(self, host, user, passwd, db, charset='utf8', port=3306):
         self.db = pymysql.connect(host=host,
                                   user=user,
                                   passwd=passwd,
                                   db=db,
                                   charset=charset,
-                                  port=port
-                                )
+                                  port=port)
         self.cursor = self.db.cursor()
     
     def get_dbs(self):
@@ -23,47 +23,49 @@ class BuildMysqlDoc:
         self.cursor.execute(sql)
         tables = []
         for table in self.cursor.fetchall():
-            tables.append({'name':table[0],'engine':table[1],'collection':table[14],'comment':table[17]})
+            tables.append({'name': table[0], 'engine': table[1], 'collection': table[14], 'comment': table[17]})
         return tables
 
-    def get_cloumns(self,table):
+    def get_cloumns(self, table):
         sql = 'show full columns from '+table
         self.cursor.execute(sql)
         columns = []
         for column in self.cursor.fetchall():
             columns.append({
-                'name':str(column[0]),
-                'comment':str(column[8]),
-                'type':str(column[1]),
-                'isnull':str(column[3]),
-                'default':str(column[5]),
-                'collection':str(column[2]),
+                'name': str(column[0]),
+                'comment': str(column[8]),
+                'type': str(column[1]),
+                'isnull': str(column[3]),
+                'default': str(column[5]),
+                'collection': str(column[2]),
             })
         return columns
+
+
 if __name__ == '__main__':
-    host    = input('ip地址(localhost):')
-    user    = input('用户名(root):')
-    passwd  = input('密码:')
-    db      = input('数据库:')
+    host = input('ip地址(localhost):')
+    user = input('用户名(root):')
+    passwd = input('密码:')
+    db = input('数据库:')
     charset = input('字符集(utf8):')
-    port    = input('端口(3306):')
+    port = input('端口(3306):')
     if not db or not passwd:
         print('请输入正确的信息:密码,数据库')
         exit
-    host    = host    if host    else 'localhost'
-    user    = user    if user    else 'root'
+    hos = host if host else 'localhost'
+    user = user if user else 'root'
     charset = charset if charset else 'utf8'
-    port    = port    if port    else 3306
+    port = port if port else 3306
 
-    build_mysql_doc = BuildMysqlDoc(host,user,passwd,db,charset,port)
+    build_mysql_doc = BuildMysqlDoc(host, user, passwd, db, charset, port)
     # print(build_mysql_doc.get_tables())
     fields = {
-        'name':'字段',
-        'comment':'注释',
-        'type':'类型',
-        'isnull':'能否为null',
-        'default':'默认值',
-        'collectio':'字符集',
+        'name': '字段',
+        'comment': '注释',
+        'type': '类型',
+        'isnull': '能否为null',
+        'default': '默认值',
+        'collectio': '字符集',
     }
     field_str = ' | '.join(list(fields.values()))
     table_str = '---|'*len(fields)
